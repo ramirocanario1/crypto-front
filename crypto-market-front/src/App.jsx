29c28601be756bfb85008a1dc21d40c052e6fc0e
ramiro <ramirocanario1@gmail.com>
2024-05-03 19:28:29 -0300

    feat(rutas): estructura base de rutas

---
+++ crypto-market-front/src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './routes/Layout';
+import Home from './routes/Home';
+import NoMatch from './routes/NoMatch';
+import Portafolio from './routes/Portafolio';
+import DetalleCripto from './routes/DetalleCripto';
+
+export default function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="portafolio" element={<Portafolio />} />
+          <Route path="cripto/:id" element={<DetalleCripto />} />
+
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+  );
+}
+
+function About() {
+  return (
+    <div>About</div>
+  )
+}
+function Contact() {
+  return (
+    <div>Contact</div>
+  )
+}
---
+++ crypto-market-front/src/routes/NoMatch.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NoMatch() {
+  return (
+    <div>Ups! Error 404</div>
+  )
+}
