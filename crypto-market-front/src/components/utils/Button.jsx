14430fbc6c8353b9be1b8a2a19b85f7238119efa
ramiro <ramirocanario1@gmail.com>
2024-05-10 18:16:29 -0300

    feat(botones): botones agregados para usar en la aplicacion

---
+++ crypto-market-front/src/components/utils/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Button({ children, type = "success" }) {
+
+  let background, hoverBg
+  if (type === "success") {
+    background = "bg-green-500"
+    hoverBg = "hover:bg-green-600"
+  } else if (type === "danger") {
+    background = "bg-red-500"
+    hoverBg = "hover:bg-red-600"
+  } else if (type === "warning") {
+    background = "bg-yellow-500"
+    hoverBg = "hover:bg-yellow-600"
+  }
+
+  return (
+    <button className={`${background} w-full p-2 rounded-md mt-5 disabled:bg-gray-600 ${hoverBg} transition-colors`}>
+      {children}
+    </button>
+  );
+}
---
+++ crypto-market-front/src/components/utils/ButtonOutlined.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function ButtonOutlined({ children, type = "success" }) {
+
+  let color, border;
+  if (type === "success") {
+    color = "text-green-500";
+    border = "border-green-500";
+  } else if (type === "danger") {
+    color = "text-red-500";
+    border = "border-red-500";
+  } else if (type === "warning") {
+    color = "text-yellow-500";
+    border = "border-yellow-500";
+  }
+
+  return (
+    <button className={`${color} ${border} border-2 w-full p-2 rounded-md mt-5 disabled:border-gray-600`}>
+      {children}
+    </button>
+  );
+
+  return <div>ButtonOutlined</div>;
+}
