ad1d3561ee4cbbb670a823046fc2c6ea8381b2ae
ramiro <ramirocanario1@gmail.com>
2024-05-11 17:11:06 -0300

    fix(login): mejora en mensajes de validador

--- crypto-market-front/src/components/login/loginValidator.js
+++ crypto-market-front/src/components/login/loginValidator.js
@@ -4,13 +4,13 @@ const schema = yup
   .object({
     email: yup
       .string()
-      .email("El email no es válido")
-      .required("Debes ingresar un email para iniciar sesion."),
+      .required("Debes ingresar un email.")
+      .email("El email no es válido"),
     password: yup
       .string()
+      .required("Debes ingresar una contraseña.")
       .min(4, "La contraseña debe tener entre 4 y 20 caracteres")
       .max(20, "La contraseña debe tener entre 4 y 20 caracteres")
-      .required("Debes ingresar una contraseña para iniciar sesion.")
       .label("Password"),
   })
   .required();
