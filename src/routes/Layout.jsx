a70cc7efdd74116372b135a2be487f8602565878
ramirocanario1 <ramirocanario1@gmail.com>
2024-06-09 13:59:51 -0300

    style: contenido centrado y con fondo oscuro

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -5,12 +5,14 @@ import Footer from '../components/layout/Footer';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col max-w-2xl">
-      <Header />
-      <main className="flex-grow bg-gray-600 text-white">
-        <Outlet />
-      </main>
-      <Footer />
-    </div> 
+    <div className='bg-gray-950 grid place-content-center'>
+      <div className="min-h-screen flex flex-col max-w-2xl min-w-96">
+        <Header />
+        <main className="flex-grow bg-gray-600 text-white">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
 }
