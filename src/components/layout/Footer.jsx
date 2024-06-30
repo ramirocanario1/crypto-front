76f3398e15a1449618a3886f434c1153e44c59a0
ramirocanario1 <ramirocanario1@gmail.com>
2024-06-30 20:10:21 -0300

    style: ajustes de estilos

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,29 +7,28 @@ import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="fixed inset-x-0 bottom-0">
-      <div className='bg-gray-800 py-4 shadow-md'>
+    <footer className="fixed inset-x-0 bottom-0 bg-gray-950 shadow-md">
+      <div className='max-w-lg mx-auto bg-gray-800 py-4'>
         <div className='grid grid-cols-5'>
-          
           <Acceso label='Movimientos' to='movimientos'>
-            <FaMoneyBill1Wave className='w-6 h-6'/>
+            <FaMoneyBill1Wave className='w-6 h-6' />
           </Acceso>
           <Acceso label='Portafolio' to='portafolio'>
-            <FaWallet className='w-6 h-6'/>
+            <FaWallet className='w-6 h-6' />
           </Acceso>
           <Acceso label='Inicio' to='/'>
-            <FaHome className='w-6 h-6'/>
+            <FaHome className='w-6 h-6' />
           </Acceso>
           <Acceso label='Depositar' to='depositar'>
-            <FaArrowDown className='w-6 h-6'/>
+            <FaArrowDown className='w-6 h-6' />
           </Acceso>
           <Acceso label='Retirar' to='retirar'>
-            <FaArrowUp className='w-6 h-6'/>
+            <FaArrowUp className='w-6 h-6' />
           </Acceso>
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 function Acceso({ to = '', children = '', label = '' }) {
