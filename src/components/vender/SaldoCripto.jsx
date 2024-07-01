85db7c116f1ff85ba6e0d526606aebe183644f2c
ramirocanario1 <ramirocanario1@gmail.com>
2024-07-01 19:48:41 -0300

    feat: skeleton agregado

--- src/components/vender/SaldoCripto.jsx
+++ src/components/vender/SaldoCripto.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Section from '../deposito/Section'
+import Skeleton from 'react-loading-skeleton'
 
 export default function SaldoCripto({ saldo, precio, cripto, isLoading }) {
   return (
     <Section className='bg-gray-700 text-white flex flex-col gap-1 p-4 text-center'>
       <p>Tu saldo disponible de {cripto.name} es</p>
       <p className='text-3xl'>
-        {isLoading ? <Skeleton width={150} height={30} /> : saldo}
+        {isLoading ? <Skeleton width={200} height={30} /> : saldo}
       </p>
     </Section>
   )
